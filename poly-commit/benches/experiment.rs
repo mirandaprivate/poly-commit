fd12a5d6c7daa4b513f17dfa1c2f58f9f0b40157
CONG MINGSHU <miranda@CONGdeMacBook-Pro.local>
2024-11-24 23:13:42 +0800

    Add memory measurement

--- poly-commit/benches/experiment.rs
+++ poly-commit/benches/experiment.rs
@@ -36,8 +36,8 @@ use ark_ff::PrimeField;
 
 use rand_chacha::ChaCha20Rng;
 
-const MIN_NUM_VARS: usize = 20;
-const MAX_NUM_VARS: usize = 21;
+const MIN_NUM_VARS: usize = 22;
+const MAX_NUM_VARS: usize = 23;
 const BIT_WIDTH: usize = 8;
 
 fn main() -> io::Result<()> {
@@ -80,18 +80,76 @@ fn main() -> io::Result<()> {
         println!("Running experiments for 2e{} variables", num_vars);
 
         experiment_smart(num_vars);
+
+
+        let mut max = max_memory.lock().unwrap();
+        println!("\n ****Max RAM: {} KB\n", *max);
+        println!("\n\n***********************************************");
+        *max = 0;
+        std::mem::drop(max);
+
+
         experiment_brakedown(num_vars);
+
+        let mut max = max_memory.lock().unwrap();
+        println!("\n ****Max RAM: {} KB\n", *max);
+        println!("\n\n***********************************************");
+        *max = 0;
+        std::mem::drop(max);
+
+
         experiment_hyrax(num_vars);
+
+        let mut max = max_memory.lock().unwrap();
+        println!("\n ****Max RAM: {} KB\n", *max);
+        println!("\n\n***********************************************");
+        *max = 0;
+        std::mem::drop(max);
+
+
         experiment_lingero(num_vars);
+
+        let mut max = max_memory.lock().unwrap();
+        println!("\n ****Max RAM: {} KB\n", *max);
+        println!("\n\n***********************************************");
+        *max = 0;
+        std::mem::drop(max);
+
+
+
         experiment_kzg(num_vars);
+
+        let mut max = max_memory.lock().unwrap();
+        println!("\n ****Max RAM: {} KB\n", *max);
+        println!("\n\n***********************************************");
+        *max = 0;
+        std::mem::drop(max);
+
+
         experiment_marlin(num_vars);
+
+        let mut max = max_memory.lock().unwrap();
+        println!("\n ****Max RAM: {} KB\n", *max);
+        println!("\n\n***********************************************");
+        *max = 0;
+        std::mem::drop(max);
+
+
         experiment_sonic(num_vars);
+
+        let mut max = max_memory.lock().unwrap();
+        println!("\n ****Max RAM: {} KB\n", *max);
+        println!("\n\n***********************************************");
+        *max = 0;
+        std::mem::drop(max);
+
+
         experiment_ipa(num_vars);
+
+        let max = max_memory.lock().unwrap();
+        println!("\n ****Max RAM: {} KB\n", *max);
+        println!("\n\n***********************************************");
     }
-
-
-    let max = max_memory.lock().unwrap();
-    println!("\n ****Max RAM: {} KB\n", *max);
 
     Ok(())
 }
